4f13386ff2b1b42dc4af0134b1a4650517675453
frameartist <seanxionghk@gmail.com>
2025-11-19 15:58:29 +0800

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "app.coreply.coreplyapp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "2.2.2"
+        versionCode = 13
+        versionName = "2.3.0-beta"
         vectorDrawables.useSupportLibrary = true
     }
     compileOptions {
