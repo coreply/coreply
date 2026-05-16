4493c9a22427186ca9b7c158e4d2c0e4db27f6c3
seaxng <253018267+seaxng@users.noreply.github.com>
2026-05-16 17:57:48 +0800

    bump version

--- coreply-android/app/build.gradle.kts
+++ coreply-android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "app.coreply.coreplyapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "2.3.5"
+        versionCode = 21
+        versionName = "2.3.6"
         vectorDrawables.useSupportLibrary = true
     }
     buildFeatures {
