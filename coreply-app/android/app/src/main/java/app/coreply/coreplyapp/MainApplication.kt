cbd6ebfc13372181a64e8527c084bc5cab20e2b1
seaxng <253018267+seaxng@users.noreply.github.com>
2026-06-02 01:30:34 +0800

    add expo

---
+++ coreply-app/android/app/src/main/java/app/coreply/coreplyapp/MainApplication.kt
@@ -0,0 +1,66 @@
+/**
+ * coreply
+ *
+ * Copyright (C) 2024 coreply
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+package app.coreply.coreplyapp
+
+import android.app.Application
+import android.content.res.Configuration
+
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
+import com.facebook.react.ReactPackage
+import com.facebook.react.ReactHost
+import com.facebook.react.common.ReleaseLevel
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+
+import expo.modules.ApplicationLifecycleDispatcher
+import expo.modules.ExpoReactHostFactory
+
+import app.coreply.coreplyapp.utils.PreferenceHelper
+
+/**
+ * Created on 1/25/17.
+ */
+open class MainApplication : Application(), ReactApplication {
+    override val reactHost: ReactHost by lazy {
+        ExpoReactHostFactory.getDefaultReactHost(
+            context = applicationContext,
+            packageList =
+                PackageList(this).packages.apply {
+                    // Packages that cannot be autolinked yet can be added manually here, for example:
+                    // add(MyReactNativePackage())
+                }
+        )
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        PreferenceHelper.init(this)
+        DefaultNewArchitectureEntryPoint.releaseLevel = ReleaseLevel.STABLE
+        loadReactNative(this)
+        ApplicationLifecycleDispatcher.onApplicationCreate(this)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
+    }
+}
